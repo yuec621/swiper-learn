20038ec1c4a651a30be3406004728856ae99495c
yuec621 <yuec621@outlook.com>
2019-04-27 16:57:08 +0800

    commit-swiper

---
+++ test/js/scenicSpot.js
@@ -0,0 +1,15 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    //   direction: 'vertical', // 垂直切换选项
+    //方向默认水平
+      loop: true, // 循环模式选项
+      autoplay:true,
+      
+      // 如果需要分页器
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      
+                 
+    })
+    
+
